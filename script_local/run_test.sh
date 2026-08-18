6a9922fb669de46af9917640fb58475a3b49a9f1
Gaia Meola <gaia.meola@gmail.com>
2026-08-18 12:59:37 +0200

    Aggiustamento di run_test.sh

--- script_local/run_test.sh
+++ script_local/run_test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 
-# legge NUM_WORKERS da .env
 if [ -f .env ]; then
     ENV_NUM_WORKERS=$(grep -E "^[[:space:]]*NUM_WORKERS[[:space:]]*=" .env | cut -d '=' -f 2- | tr -d ' ')
 fi
@@ -10,6 +9,6 @@ NUM_WORKERS="${ENV_NUM_WORKERS:-2}"
 echo "[RUN_TEST] Avvio con NUM_WORKERS=$NUM_WORKERS (da .env)..."
 
 docker compose down
-docker compose up -d --scale worker=$NUM_WORKERS
+docker compose up -d --scale worker=$NUM_WORKERS --scale orchestrator=2
 docker compose run --rm test-engine
 docker compose down
